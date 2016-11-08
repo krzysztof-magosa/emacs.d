628dac83777f019d2c6a23541e2de62c847ce203
Krzysztof Magosa <krzysztof@magosa.pl>
2016-11-08 22:15:03 +0100

    fix keyboard in cocoa emacs

--- init/init-keyboard.el
+++ init/init-keyboard.el
@@ -5,6 +5,10 @@
 (define-key global-map (kbd "M-[ H") 'beginning-of-line)
 (define-key global-map (kbd "M-[ F") 'end-of-line)
 
+;; cmd+left/right arrow in cocoa
+(define-key global-map (kbd "s-<left>") 'beginning-of-line)
+(define-key global-map (kbd "s-<right>") 'end-of-line)
+
 ;; jumping between windows
 (define-key global-map (kbd "C-<up>") 'windmove-up)
 (define-key global-map (kbd "C-<down>") 'windmove-down)
@@ -12,5 +16,9 @@
 (define-key global-map (kbd "C-<right>") 'windmove-right)
 
 (define-key global-map (kbd "C-c C-c") 'comment-or-uncomment-region)
+
+;; fix right alt on OS X
+(when (eq system-type 'darwin)
+  (setq mac-right-option-modifier 'none))
 
 (provide 'init-keyboard)
