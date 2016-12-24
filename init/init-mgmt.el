4adfb3de627567ed4bb8b1dd489af39589237a37
Krzysztof Magosa <krzysztof@magosa.pl>
2016-12-24 15:40:24 +0100

    remove unused plugin

--- init/init-mgmt.el
+++ init/init-mgmt.el
@@ -17,16 +17,4 @@
     (setq projectile-completion-system 'helm)
     (setq projectile-enable-idle-timer t)))
 
-;; it must be here on the bottom, because of bug with cl lib
-(use-package fiplr
-  :ensure t
-  :defer t
-  :bind("C-c o" . fiplr-find-file)
-  :config
-  (progn
-    (setq fiplr-ignored-globs '(
-      (directories (".git" ".svn" ".sass-cache" "node_modules"))
-      (files ("*.jpg" "*.png" "*.zip" "*.svg" "*.eot" "*.woff" "*.ttf" "*~"))))
-    (setq fiplr-root-markers '(".project-root"))))
-
 (provide 'init-mgmt)
