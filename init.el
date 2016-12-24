43448512474cd6c1ab1d7cc5d9d8eb0e763644af
Krzysztof Magosa <krzysztof@magosa.pl>
2016-12-24 14:56:46 +0100

    store custom settings in separate file

--- init.el
+++ init.el
@@ -32,16 +32,6 @@
 (require 'init-keyboard)
 (require 'init-appearance)
 (require 'init-aliases)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ecb-options-version "2.40")
- '(paradox-github-token t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
