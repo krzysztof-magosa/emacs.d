bdffa492ff693befc17ca10dcdcba0c8aadc09fc
Krzysztof Magosa <krzysztof@magosa.pl>
2017-03-19 22:33:17 +0100

    commit autogenerated variable of emacs 25.x

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode whitespace-cleanup-mode which-key web-mode vue-mode use-package switch-window swift-mode sublimity spaceline smooth-scrolling smeargle scss-mode ruby-additional rubocop rainbow-mode rainbow-identifiers rainbow-delimiters puppet-mode polymode pip-requirements php-mode password-generator osx-clipboard nlinum nginx-mode neotree move-text monokai-theme minimap markdown-mode magit json-mode js2-mode jinja2-mode iedit hungry-delete highlight-symbol highlight-parentheses highlight-numbers highlight-current-line helm-projectile helm-gtags helm-git-grep helm-fuzzier helm-flycheck helm-flx helm-descbinds helm-codesearch haxor-mode gitignore-mode gitconfig-mode git-timemachine git-gutter expand-region evil-numbers erlang elpy editorconfig dockerfile-mode dashboard crontab-mode company-statistics company-sourcekit company-quickhelp company-jedi company-go company-c-headers company-ansible company-anaconda cmake-mode cmake-ide cheatsheet cff buffer-move bison-mode benchmark-init beacon bats-mode apache-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
