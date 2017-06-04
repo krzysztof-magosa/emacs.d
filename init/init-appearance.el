1c76272f1193481e85e0a1ed55cd4fe58aef95ec
Krzysztof Magosa <krzysztof.magosa@performgroup.com>
2017-06-04 16:53:40 +0200

    use smart-mode-line instead of spaceline

--- init/init-appearance.el
+++ init/init-appearance.el
@@ -47,47 +47,14 @@
     (add-hook 'yaml-mode-hook 'highlight-indentation-mode)
     (add-hook 'c++-mode-hook (lambda () (setq highlight-indentation-offset 2)))))
 
-(use-package spaceline
+(use-package smart-mode-line
   :ensure t
-  :config
+  :defer t
+  :init
   (progn
-    (require 'spaceline-config)
-    (spaceline-emacs-theme)
-    (spaceline-helm-mode)
-    (spaceline-toggle-persp-name-off)
-    (spaceline-toggle-workspace-number-off)
-    (spaceline-toggle-window-number-off)
-    (spaceline-toggle-evil-state-off)
-    (spaceline-toggle-anzu-off)
-    (spaceline-toggle-auto-compile-off)
-    (spaceline-toggle-buffer-modified-on)
-    (spaceline-toggle-buffer-size-off)
-    (spaceline-toggle-buffer-id-on)
-    (spaceline-toggle-remote-host-on)
-    (spaceline-toggle-major-mode-off)
-    (spaceline-toggle-flycheck-error-on)
-    (spaceline-toggle-flycheck-warning-on)
-    (spaceline-toggle-flycheck-info-off)
-    (spaceline-toggle-minor-modes-off)
-    (spaceline-toggle-process-off)
-    (spaceline-toggle-erc-track-off)
-    (spaceline-toggle-version-control-on)
-    (spaceline-toggle-org-pomodoro-off)
-    (spaceline-toggle-org-clock-off)
-    (spaceline-toggle-nyan-cat-off)
-    (spaceline-toggle-battery-off)
-    (spaceline-toggle-which-function-off)
-    (spaceline-toggle-python-pyvenv-off)
-    (spaceline-toggle-python-pyenv-off)
-    (spaceline-toggle-paradox-menu-off)
-    (spaceline-toggle-selection-info-off)
-    (spaceline-toggle-input-method-off)
-    (spaceline-toggle-buffer-encoding-abbrev-off)
-    (spaceline-toggle-point-position-off)
-    (spaceline-toggle-line-column-on)
-    (spaceline-toggle-global-on)
-    (spaceline-toggle-buffer-position-on)
-    (spaceline-toggle-hud-off)))
+    (setq sml/theme 'respectful)
+    (setq sml/no-confirm-load-theme t)
+    (sml/setup)))
 
 (use-package which-key
   :ensure t
